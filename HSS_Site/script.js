84994a35d8f62a1976db72197ec503648c8f129a
Michael <m.anderjaska@gmail.com>
2017-11-29 20:06:53 -0500

    submission button done - needs styling

--- HSS_Site/script.js
+++ HSS_Site/script.js
@@ -1,6 +1,8 @@
+var gloBal = []
+
 $(document).ready(function () {
 
-    var board = [];
+    board = [];
     let url = 'http://localhost:3000/newBoard';
 
 
@@ -12,7 +14,12 @@ $(document).ready(function () {
     })
 
 
+
+
 });
+//
+
+
 
 function drawNewBoard (data) {
     board = data.board;
@@ -22,13 +29,69 @@ function drawNewBoard (data) {
         $('table.sudoku').append("<tr id='" + rowID + "'></tr>");
 
         for (let col = 0; col < board[0].length; col++) {
-            let cellID = row + ',' + col;
+            let cellID = row + "_" + col;
 
             if (board[row][col] == null) {
-                $('tr#' +rowID).append("<td id='" + cellID + "'></tr>");
+                $('tr#' +rowID).append("<td id='" + cellID + "'><input type='text'></tr>");
             } else {
                 $('tr#' +rowID).append("<td id='" + cellID + "'>" + board[row][col] + "</tr>");
             }
         }
     }
+
+    //return(submitBoard())
+}
+
+
+
+function submitBoard(){
+    //extract numbers from the table
+    //if not a number, stop, alert
+    //once done, pass to get request call back
+    //if successful, then toast "Correct!" or similar
+
+    sudokuEntries = {'board':[]};
+    board = []
+
+    for (let row = 0; row < 9; row++) {
+        entryRow = []
+        for (let col = 0; col < 9; col++) {
+            let cellId = row + "_" + col;
+            let entry = $("tr #" + cellId);
+
+            if(entry.children().length > 0 ){
+                let inputVal = $("tr #" + cellId + " input").val();
+                if(inputVal === ""){
+                    //alert("please fill out the board");
+                } else if (!(/^\d+$/.test(inputVal)) ){
+                    alert("Only numbers please :)");
+                    return;
+                } else {
+                    entryRow.push(inputVal);
+                }
+
+            } else {
+                entryRow.push(entry.text());
+            }
+        }
+
+        board.push(entryRow);
+    }
+
+    sudokuEntries['board'] = board
+
+    //pass to server
+    /*let url = 'http://localhost:3000/newBoard';
+
+    $.ajax({
+        url: url,
+        success: submissionResults,
+        data: null,
+        dataType: 'json'
+    })*/
+
+}
+
+function submissionResults(board){
+    //if true, then alert "Congrats!", otherwise, alert "try again!"
 }
