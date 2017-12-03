ebf943bf7996c1e6ff05a7b6d8af2eb52f0e4161
Michael <m.anderjaska@gmail.com>
2017-12-02 22:17:40 -0500

    modularized, as this makes it better for testing.

--- HSS_Server/test.hs
+++ HSS_Server/test.hs
@@ -1,80 +1,24 @@
 -- tests the validator and solver functions
-emptyBoard :: Board
-emptyBoard = Board [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just False)
+import testData
+import server
 
-
-
-duplicateInRow :: Board
-duplicateInRow = Board [[Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Just 1],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Just 3, Nothing, Nothing, Just 3, Nothing, Just 5, Nothing, Just 4, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Just 2, Nothing, Nothing, Nothing, Just 2, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Just 7, Nothing, Just 6, Nothing, Nothing, Nothing, Just 6, Nothing],
-[Nothing, Nothing, Just 8, Nothing, Nothing, Just 8, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Just 4, Nothing, Nothing,  Nothing, Just 4, Nothing],
-[Nothing, Just 9, Nothing, Nothing, Nothing, Nothing, Nothing, Just 9, Nothing]] (Just False)
-
-
-duplicateInCol :: Board
-duplicateInCol = Board [[Just 1, Just 2, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Just 2, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just False)
-
-solvedBoard :: Board
-sovledBoard = Board [[Just 1,Just 2,Just 3,Just 4,Just 5,Just 6,Just 7,Just 8,Just 9],
-[Just 4,Just 5,Just 6,Just 7,Just 8,Just 9,Just 1,Just 2,Just 3],
-[Just 7,Just 8,Just 9,Just 1,Just 2,Just 3,Just 4,Just 5,Just 6],
-[Just 2,Just 1,Just 4,Just 3,Just 6,Just 5,Just 8,Just 9,Just 7],[
-Just 3,Just 6,Just 5,Just 8,Just 9,Just 7,Just 2,Just 1,Just 4],[Just 8,Just 9,Just 7,Just 2,Just 1,Just 4,Just 3,Just 6,Just 5],[Just 5,Just 3,Just 1,Just 6,Just 4,Just 2,Just 9,Just 7,Just 8],[Just 6,Just 4,Just 2,Just 9,Just 7,Just 8,Just 5,Just 3,Just 1],[Just 9,Just 7,Just 8,Just 5,Just 3,Just 1,Just 6,Just 4,Just 2]] (Just True)
-
-solvableBoard :: Board
-solvableBoard = Board [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just True)
-
-solvableBoard :: Board
-solvableBoard = Board [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
-[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just True)
+checkRowsTest :: Bool
+checkRowsTest =
+    let Board b v = duplicateInRow in
+        isValid(b) == False
 
 unitTests = [
-  ("checkRows", checkRowsTest),
-  ("checkCols", checkColsTest),
-  ("checkCells", checkCellsTest),
-  ("solvePossible", solvePossibleTest),
-  ("solveImpossible", solveImpossibleTest)
-  ]
+  ("checkRows", checkRowsTest)]
+ -- ("checkCols", checkColsTest),
+  --("checkCells", checkCellsTest),
+ -- ("checkSolvedBoard", checkSolvedBoardTest)
+ -- ("solvePossible", solvePossibleTest),
+ -- ("solveImpossible", solveImpossibleTest)
+  --]
 
-runPublicTests :: String -> IO ()
-runPublicTests func =
-  case (lookup func publicTests) of
+runTests :: String -> IO ()
+runTests func =
+  case (lookup func unitTests) of
     Just t -> if t then putStrLn $ "Passed " ++ func
               else error $ "Failed " ++ func
     Nothing -> error $ "Unrecognized test: " ++ func
@@ -82,5 +26,5 @@ runPublicTests func =
 main = do
   args <- getArgs
   let func = head args in
-    if ("public" `isPrefixOf` func) then runPublicTests func
+    if ("public" `isPrefixOf` func) then runTests func
     else error $ "No prefix \"public\" in " ++ func
---
+++ HSS_Server/testHelper.hs
@@ -0,0 +1,34 @@
+module TestHelper
+( emptyBoard
+, duplicateInRow
+, duplicateInCol
+, duplicateInCell
+, solvedBoard
+, solvableBoard
+, unsolvableBoard
+) where
+
+import Main
+
+emptyBoard :: Board
+emptyBoard = Board [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just False)
+
+
+duplicateInRow :: Board
+duplicateInRow = Board [[Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Just 1],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Just 3, Nothing, Nothing, Just 3, Nothing, Just 5, Nothing, Just 4, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Just 2, Nothing, Nothing, Nothing, Just 2, Nothing, Nothing, Nothing, Nothing],[Nothing, Just 7, Nothing, Just 6, Nothing, Nothing, Nothing, Just 6, Nothing],[Nothing, Nothing, Just 8, Nothing, Nothing, Just 8, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Just 4, Nothing, Nothing,  Nothing, Just 4, Nothing],[Nothing, Just 9, Nothing, Nothing, Nothing, Nothing, Nothing, Just 9, Nothing]] (Just False)
+
+
+duplicateInCol :: Board
+duplicateInCol = Board [[Just 1, Just 2, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Just 2, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just False)
+
+duplicateInCell :: Board
+duplicateInCol = Board [[Just 1, Nothing , Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Just 2, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just False)
+
+solvedBoard :: Board
+solvedBoard = Board [[Just 1,Just 2,Just 3,Just 4,Just 5,Just 6,Just 7,Just 8,Just 9],[Just 4,Just 5,Just 6,Just 7,Just 8,Just 9,Just 1,Just 2,Just 3],[Just 7,Just 8,Just 9,Just 1,Just 2,Just 3,Just 4,Just 5,Just 6],[Just 2,Just 1,Just 4,Just 3,Just 6,Just 5,Just 8,Just 9,Just 7],[Just 3,Just 6,Just 5,Just 8,Just 9,Just 7,Just 2,Just 1,Just 4],[Just 8,Just 9,Just 7,Just 2,Just 1,Just 4,Just 3,Just 6,Just 5],[Just 5,Just 3,Just 1,Just 6,Just 4,Just 2,Just 9,Just 7,Just 8],[Just 6,Just 4,Just 2,Just 9,Just 7,Just 8,Just 5,Just 3,Just 1],[Just 9,Just 7,Just 8,Just 5,Just 3,Just 1,Just 6,Just 4,Just 2]] (Just True)
+
+solvableBoard :: Board
+solvableBoard = Board [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just True)
+
+unsolvableBoard :: Board
+solvableBoard = Board [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just True)
