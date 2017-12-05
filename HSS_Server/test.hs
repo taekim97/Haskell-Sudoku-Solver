c5e95796e944bc81349f93c1fe11fbefd4e3b4f4
TERRY <taekim97@gmail.com>
2017-12-05 00:42:40 -0500

    Got rid of testHelper

--- HSS_Server/test.hs
+++ HSS_Server/test.hs
@@ -4,10 +4,27 @@ import System.Environment
 import Data.Monoid
 import Debug.Trace
 
-
-
-import TestHelper
 import Sudoku
+
+duplicateInRow :: Board
+duplicateInRow = Board [[Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Just 1],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Just 3, Nothing, Nothing, Just 3, Nothing, Just 5, Nothing, Just 4, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Just 2, Nothing, Nothing, Nothing, Just 2, Nothing, Nothing, Nothing, Nothing],[Nothing, Just 7, Nothing, Just 6, Nothing, Nothing, Nothing, Just 6, Nothing],[Nothing, Nothing, Just 8, Nothing, Nothing, Just 8, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Just 4, Nothing, Nothing,  Nothing, Just 4, Nothing],[Nothing, Just 9, Nothing, Nothing, Nothing, Nothing, Nothing, Just 9, Nothing]] (Just False)
+
+
+duplicateInCol :: Board
+duplicateInCol = Board [[Just 1, Just 2, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Just 2, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just False)
+
+duplicateInCell :: Board
+duplicateInCell = Board [[Just 1, Nothing , Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Just 2, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Just 1, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just False)
+
+solvedBoard :: Board
+solvedBoard = Board [[Just 1,Just 2,Just 3,Just 4,Just 5,Just 6,Just 7,Just 8,Just 9],[Just 4,Just 5,Just 6,Just 7,Just 8,Just 9,Just 1,Just 2,Just 3],[Just 7,Just 8,Just 9,Just 1,Just 2,Just 3,Just 4,Just 5,Just 6],[Just 2,Just 1,Just 4,Just 3,Just 6,Just 5,Just 8,Just 9,Just 7],[Just 3,Just 6,Just 5,Just 8,Just 9,Just 7,Just 2,Just 1,Just 4],[Just 8,Just 9,Just 7,Just 2,Just 1,Just 4,Just 3,Just 6,Just 5],[Just 5,Just 3,Just 1,Just 6,Just 4,Just 2,Just 9,Just 7,Just 8],[Just 6,Just 4,Just 2,Just 9,Just 7,Just 8,Just 5,Just 3,Just 1],[Just 9,Just 7,Just 8,Just 5,Just 3,Just 1,Just 6,Just 4,Just 2]] (Just True)
+
+solvableBoard :: Board
+solvableBoard = Board [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just True)
+
+unsolvableBoard :: Board
+unsolvableBoard = Board [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Just 9, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]] (Just True)
+
 
 --Tests if validator can reject based on rows
 checkRowsTest :: Bool
